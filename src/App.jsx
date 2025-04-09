dc803987fe5b32c8b6b4b1f05d67ab1b40290eb5
MiltonMrtinezDC <mdm0033384@est.univalle.edu>
2025-04-09 18:46:30 -0400

    Agrega icono de lupa y actualiza componentes y estilos

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,27 @@ function App() {
     <div>
       <Header />
       <main className="p-6">
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+     
+         {/* Logo */}
+        <div className="flex items-center space-x-2">
+          <img
+            src="/Logo.png" 
+            alt="Logo"
+            className="h-10 w-auto"
+          />
+          
+        </div>
+                {/* Barra de navegación */}
+        <nav className="hidden md:flex space-x-6 text-sm">
+          <a href="#" className="hover:underline">Acerca de nosotros</a>
+          <a href="#" className="hover:underline">Nuestros servicios</a>
+          <a href="#" className="hover:underline">Crowd Funding</a>
+          <a href="#" className="hover:underline">Apoya a emprendedor</a>
+          <a href="#" className="hover:underline">Postulaciones</a>
+        </nav>
       </main>
     </div>
+    
   );
 }
 
